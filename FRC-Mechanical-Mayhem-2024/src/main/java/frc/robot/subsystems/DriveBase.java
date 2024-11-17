ffdf60efc3fef5ac74f9c1ca2cf7039d7b7c38a4
-m <-m>
2024-11-16 17:33:49 -0800

    New drive base branch

--- FRC-Mechanical-Mayhem-2024/src/main/java/frc/robot/subsystems/DriveBase.java
+++ FRC-Mechanical-Mayhem-2024/src/main/java/frc/robot/subsystems/DriveBase.java
@@ -7,7 +7,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class DriveBase extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   public ExampleSubsystem() {}
 
