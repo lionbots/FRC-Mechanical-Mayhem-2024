d7c4d475765e2a00e16e3099e4780c8a20421688
jiayingdawnma@gmail.com <guanyewu20081023@gmail.com>
2024-11-18 17:23:40 -0800

    Import  library for Spark Max

--- FRC-Mechanical-Mayhem-2024/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ FRC-Mechanical-Mayhem-2024/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -6,6 +6,8 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class ArmSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
