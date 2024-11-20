7b9978e0432b8292001c10c771b7966b45c5dcf1
Nathan Ko <konathan005@gmail.com>
2024-11-20 09:30:10 -0800

    Set idle mode and current limit on claw motor

--- src/main/java/frc/robot/subsystems/ClawSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -8,11 +8,15 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkBase.IdleMode;
 
 public class ClawSubsystem extends SubsystemBase {
   private final CANSparkMax clawMotor = new CANSparkMax(0, MotorType.kBrushed);
   /** Creates a new ExampleSubsystem. */
-  public ClawSubsystem() {}
+  public ClawSubsystem() {
+    clawMotor.setSmartCurrentLimit(30);
+    clawMotor.setIdleMode(IdleMode.kBrake);
+  }
 
   /**
    * Example command factory method.
