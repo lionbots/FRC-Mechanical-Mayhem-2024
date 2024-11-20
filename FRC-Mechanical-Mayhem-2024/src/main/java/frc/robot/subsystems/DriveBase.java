dfc3e6e9d216afb850322a9af42fa6bc7bf41c76
-m <-m>
2024-11-19 17:15:41 -0800

    Create Drive Method

--- FRC-Mechanical-Mayhem-2024/src/main/java/frc/robot/subsystems/DriveBase.java
+++ FRC-Mechanical-Mayhem-2024/src/main/java/frc/robot/subsystems/DriveBase.java
@@ -7,7 +7,6 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 
 import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
 
@@ -17,9 +16,13 @@ public class DriveBase extends SubsystemBase {
   private final DifferentialDrive differentialDrive = new DifferentialDrive(leftWheel, rightWheel);
 
   /** Creates a new ExampleSubsystem. */
-  public DriveBase() {}
+  public DriveBase(){
+    
+  }
 
-
+  public void drive(double speed, double rotation){
+    differentialDrive.arcadeDrive(rotation, speed);
+  }
 
 
   @Override
