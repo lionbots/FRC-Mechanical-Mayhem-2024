a7a74a867d4c836381aa4eb723c300cce3f5ec8c
dawnmango <jiayingdawnma@gmail.com>
2024-11-21 08:59:39 -0800

    Rename method for the Autonomous

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -2,19 +2,18 @@
 // // Open Source Software; you can modify and/or share it under the terms of
 // // the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.commands;
+package frc.robot.commands;
 
-// import frc.robot.subsystems.ArmSubsystem;
-// import edu.wpi.first.wpilibj2.command.Command;
-// import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.ArmSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
 
-// public final class Autos {
-//   /** Example static factory for an autonomous command. */
-//   public static Command exampleAuto(ArmSubsystem subsystem) {
-//     return Commands.sequence(subsystem.exampleMethodCommand(), new MoveArmCommand(subsystem));
-//   }
+public final class Autos {
+  /** This makes the robot back out of the starting area for 1.5 seconds */
+  public static Command LeaveAutonomous() {
+    
+  }
 
-//   private Autos() {
-//     throw new UnsupportedOperationException("This is a utility class!");
-//   }
-// }
+  private Autos() {
+    throw new UnsupportedOperationException("This is a utility class!");
+  }
+}
